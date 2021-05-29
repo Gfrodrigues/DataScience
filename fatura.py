022e0ce675b39a18a865ff37e9a8210a920a8063
GUSTAVO FELIPE RODRIGUES <gfrodrigues@gmail.com.br>
2021-05-28 23:57:51 -0300

    1 commit do arquivo

---
+++ fatura.py
@@ -0,0 +1,10 @@
+nomeCliente = str(input ("Digite o nome do cliente: " ))
+diaVencimento = int(input("Digite o dia de vencimento: "))
+mesVencimento = str(input("Digite o mês de vencimento: "))
+valorFatura = input("Digite o valor da fatura: ")
+
+print("Olá, ",nomeCliente)
+print("A sua fatura com vencimento em", diaVencimento,"de",mesVencimento,"no valor de R$", valorFatura, "está fechada.")
+
+
+
